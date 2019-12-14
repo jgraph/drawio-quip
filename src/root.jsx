bb9fb4ed7055b5ab7fff970abc277604ad68c32f
Gaudenz Alder <gaudenz@jgraph.com>
2019-12-14 13:29:04 +0100

    Uses draw.io 12.4.2

--- src/root.jsx
+++ src/root.jsx
@@ -2400,12 +2400,6 @@ function main(isCreateViewer, onLoad)
     {
         graphSetShadowVisible.apply(this, [Editor.shadowOptionEnabled && value, fireEvent]);
     };
-
-    // Disables CSS transforms for zoom
-    Graph.prototype.isFastZoomEnabled = function()
-    {
-    	return false;
-    }
     
     // Disables CSS transforms in editable documents
 	var graphIsCssTransformsSupported = Graph.prototype.isCssTransformsSupported;
